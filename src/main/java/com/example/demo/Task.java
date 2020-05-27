e8fb8d4ef14ae0a8922bf09f42fa5ee2dee17a8f
azdoolittle <drewdoolittle@gmail.com>
2020-05-27 13:57:14 -0400

    ctrl, task class

---
+++ src/main/java/com/example/demo/Task.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Date dueDate;
+
+    @NotNull
+    private String priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
